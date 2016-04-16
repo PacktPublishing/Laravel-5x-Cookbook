063b1baf80346d47a50093c17f55a8ba37413063
vagrant <vagrant@homestead>
2016-04-16 17:28:35 +0000

    Working on search

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests;
+use App\Interfaces\ComicClientInterface;
+use App\MarvelApi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Session;
 
 class HomeController extends Controller
 {
+
     /**
-     * Create a new controller instance.
-     *
-     * @return void
+     * @var MarvelApi
      */
-    public function __construct()
+    private $clientInterface;
+
+    public function __construct(ComicClientInterface $clientInterface)
     {
-        //$this->middleware('auth');
+        $this->clientInterface = $clientInterface;
     }
 
     /**
@@ -22,8 +27,42 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home.index');
+        $name = '';
+
+        if($request->input('search')){
+            $name =  $request->input('search');
+            $message = sprintf("Your results for %s", $name);
+            Session::flash('status', $message);
+        }
+
+        $results = $this->clientInterface->comics($name);
+
+        $results = $this->transformResults($results);
+
+        return Response::view('home.index', compact('results'));
+    }
+
+    private function transformResults($results)
+    {
+        if(isset($results['data']))
+            return $results['data'];
+        
+        return $this->returnEmptyResults();
+    }
+    
+    private function returnEmptyResults()
+    {
+        $results = [
+            'results'   => [],
+            'offset'    => 0,
+            'limit'     => 20,
+            'total'     => 0,
+            'count'     => 0
+        ];
+        
+        return $results;
+        
     }
 }
--- resources/views/home/_search.blade.php
+++ resources/views/home/_search.blade.php
@@ -1,6 +1,7 @@
 <form action="{{ route('home') }}" method="GET" class="form-horizontal">
     <div class="input-group">
-        <input type="text" name="search" class="form-control col-lg-10" placeholder="Search for your comic...">
+        <input type="text" name="search" class="form-control col-lg-10"
+               placeholder="Search for your comic...">
         <span class="input-group-btn">
             <button type="submit" class="btn btn-primary">Search</button>
         </span>
