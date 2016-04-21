0bc69136b05a784d0ed0543821fac950f62dfce7
Alfred Nutile <me@alfrednutile.info>
2016-04-21 17:51:38 -0400

    Chapter 5: Factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -14,7 +14,8 @@
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->safeEmail,
+        'email' => $faker->email,
+        'twitter' => str_random(10),
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
